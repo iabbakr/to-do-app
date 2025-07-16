08b88d0d266c498980ad6ee117f2aa79a60acf9d
iabbakr <abubakaribrahim603@gmail.com>
2025-07-16 12:44:35 +0100

    added css on app.css

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,15 @@ function App() {
   
 
   return (
-    <>
+    <main className="app">
+      <h1 className="app-title">ToDo App</h1>
+      <p className="app-description">Manage your tasks efficiently</p>
+      <p className="app-note">Note: This is a simple ToDo app built with React.</p>
+      <p className="app-note">You can add, delete, and clear tasks.</p>
+      <p className="app-note">Tasks are stored in memory and will not persist on page reload.</p>
+      <p className="app-note">Enjoy</p>   
      <Todo />
-    </>
+    </main>
   )
 }
 
