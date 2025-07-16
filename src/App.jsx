d7263d36165392877eb3e403ce4d300431835224
iabbakr <abubakaribrahim603@gmail.com>
2025-07-16 12:48:58 +0100

    notes

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,7 @@ function App() {
     <main className="app">
       <h1 className="app-title">ToDo App</h1>
       <p className="app-description">Manage your tasks efficiently</p>
-      <p className="app-note">Note: This is a simple ToDo app built with React.</p>
-      <p className="app-note">You can add, delete, and clear tasks.</p>
-      <p className="app-note">Tasks are stored in memory and will not persist on page reload.</p>
-      <p className="app-note">Enjoy</p>   
+      <p className="app-note">Note: This is a simple ToDo app built with React. You can add, delete, and clear tasks. Tasks are stored in memory and will not persist on page reload. Enjoy</p>   
      <Todo />
     </main>
   )
